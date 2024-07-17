51d3aac023c8535dd09fc37a4393cf77f3586541
Subrat kumar Sahoo <113829258+MeSubrat@users.noreply.github.com>
2024-07-17 18:36:22 +0530

    Add files via upload

---
+++ 10-07-2024/FlattenALinkedlist.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+Node* arr2LL(vector<int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+void print(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"x";
+}
+
+//OPTIMAL APPROACH
+
+
+int main()
+{
+    vector<int> arr={1,2,3,4,5};
+    Node* head=arr2LL(arr);
+    print(head);
+}
